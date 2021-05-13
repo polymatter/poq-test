a3bb3c75e95fdeaad4947ae26be0cddfd635861c
polymatter <jlake.jobs@gmail.com>
2021-05-13 22:50:11 +0100

    product shows image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Product, { ProductDetails } from './components/Product'
+
+const endpoint = 'https://run.mocky.io/v3/fca7ef93-8d86-4574-9a4a-3900d91a283e'
 
 function App() {
+
+  const [products, setProducts] = useState([] as ProductDetails[])
+
+  useEffect(() => {
+    fetch(endpoint)
+    .then(data => data.json() as Promise<ProductDetails[]>)
+    .then(setProducts)
+  }, [])
+
   return (
     <div className="App">
       Test
+      <Product {...products[0]}/>
     </div>
   );
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+type StringyBool = "TRUE" | "FALSE"
+
+export interface ProductDetails {
+    productId: number;
+    name: string;
+    description: string;
+    price: number;
+    priceWas: number;
+    available: StringyBool;
+    quantity: number;
+    lowOnStock: StringyBool;
+    promotionBadge: string;
+    imageUrl: string;
+}
+
+const Product = (product: ProductDetails) => {
+    return (
+        <section className="product">
+            <img src={`${product.imageUrl}`} alt={`image of ${product.name}` }/>
+        </section>
+    )
+}
+
+export default Product
