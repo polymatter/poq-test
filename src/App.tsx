4f2cb94a6ef32c16cf8eed14738b28d902e11f04
polymatter <jlake.jobs@gmail.com>
2021-05-14 08:30:43 +0100

    select and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
 import Checkable from './components/Checkable';
 import Product, { ProductDetails } from './components/Product'
@@ -8,21 +10,40 @@ const endpoint = 'https://run.mocky.io/v3/fca7ef93-8d86-4574-9a4a-3900d91a283e'
 function App() {
 
   const [products, setProducts] = useState([] as ProductDetails[])
+  const [selectedProducts, setSelectedProducts] = useState([] as number[])
+  const [deletedProducts, setDeletedProducts] = useState([] as number[])
 
   useEffect(() => {
     fetch(endpoint)
-    .then(data => data.json() as Promise<ProductDetails[]>)
-    .then(setProducts)
+      .then(data => data.json() as Promise<ProductDetails[]>)
+      .then(setProducts)
   }, [])
 
+  function removeProducts() {
+    setDeletedProducts(deletedProducts.concat(selectedProducts));
+  }
+
+  function selectProduct(productId:number) {
+    return (isSelected:boolean) => {
+      if (isSelected && selectedProducts.indexOf(productId) === -1) {
+        setSelectedProducts(selectedProducts.concat(productId));
+      } else if (!isSelected) {
+        setSelectedProducts(selectedProducts.filter(selectedId => selectedId != productId));
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      { products.map(product =>
-        <Checkable key={product.productId}>
-          <Product {...product}/>
-        </Checkable>
-      )}
-    </div>
+    <>
+      <Button variant="contained" color="primary" onClick={removeProducts}>Remove selected products</Button>
+      <div className="App">
+        {products.filter(product => deletedProducts.indexOf(product.productId) === -1).map(product =>
+          <Checkable key={product.productId} onCheck={selectProduct(product.productId)}>
+            <Product {...product} />
+          </Checkable>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/Checkable.tsx
+++ src/components/Checkable.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 import Checkbox from '@material-ui/core/Checkbox';
 
-const Checkable = (props: any) => {
+const Checkable = (props: { children: any, onCheck: Function }) => {
 
-    const [isChecked, setChecked] = useState(false);
+  const [isChecked, setChecked] = useState(false);
 
-    const toggleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-    }
+  const toggleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+    props.onCheck(event.target.checked);
+  }
 
-    return (
-        <div className="checkable">
-            <Checkbox checked={isChecked} onChange={toggleCheck} />
-            {props.children}
-        </div>
-    )
+  return (
+    <div>
+      <Checkbox checked={isChecked} onChange={toggleCheck} />
+      {props.children}
+    </div>
+  )
 }
 
 export default Checkable
